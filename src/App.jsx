087e68a55fe33b67576ba96a4790b26b4818b3ed
harlyon <harrisonekpobimi@gmail.com>
2023-08-19 11:36:35 +0200

    few changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <TextHeader
-        title={"I'm Fatima a Frontend Developer from Toronto"}
+        title="I'm Fatima a Frontend Developer from Toronto"
         copy="Blanditiis praesentium aliquam illum tempore incidunt debitis dolorem magni est deserunt sed qui libero. Qui voluptas amet."
         buttonText="Available for Hire"
       />

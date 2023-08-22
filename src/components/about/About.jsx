979fb6003329df7383e0e4354308b664b9a1dc35
Fatima <fatimahafeez.mh@gmail.com>
2023-08-22 13:11:10 -0400

    Updated Content

---
+++ src/components/about/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+    <section className="about__bg " id="About">
+      <div className="about__bg1">
+        <div className="about__header">
+          <h1>Hi, I'm Fatima</h1>
+          <h2>Web Developer</h2>
+        </div>
+        <div className="about__para">
+          <p>Professionally connected with the web development industry.<br/>
+            Problem solver, well-organised person, loyal employee with high attention to detail. <br/>
+            Enjoy outdoor activities, socializing and coding of course. <br/>
+            Interested in the working with challenging web projects and willing to add value to the organization.<br/>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About;

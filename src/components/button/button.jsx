8b754caf1901e70dda74a84fc61e0d409f72ba57
Fatima <fatimahafeez.mh@gmail.com>
2023-08-22 13:10:09 -0400

    Updated Button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./button.css";
 
-const Button = ({ buttonText }) => {
+const Button = () => {
   return (
-    <a href="contact.html" className="btn-get-started">
-      {buttonText}
-    </a>
+    <div className="btn-get-started">
+      <a href="contact.html"><button type="button">Available for Hire</button> </a>
+    </div>
   );
 };
 

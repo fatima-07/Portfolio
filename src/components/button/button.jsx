e6ffdf517586c0736edede9b627370eb65a8a84d
fatima-07 <110798554+fatima-07@users.noreply.github.com>
2023-08-22 14:18:03 -0400

    Update button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -4,7 +4,7 @@ import "./button.css";
 const Button = () => {
   return (
     <div className="btn-get-started">
-      <a href="contact.html"><button type="button">Available for Hire</button> </a>
+      <button type="button">Available for Hire</button> 
     </div>
   );
 };

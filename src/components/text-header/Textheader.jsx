589521705d879166c5588f6b09c31449b339d776
Fatima <fatimahafeez.mh@gmail.com>
2023-08-22 13:08:48 -0400

    Added Textheader, content and styles

---
+++ src/components/text-header/Textheader.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./text-header.css";
+import Button from "../button/Button";
+
+const TextHeader = () => {
+  return (
+    <section
+      id="hero"
+      className="hero d-flex flex-column justify-content-center align-items-center" 
+      data-aos="fade-up"
+      data-aos-delay={600} 
+    >
+     <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6 text-center"> 
+           <h1>Front-End Developer</h1>
+            <p>Resolving design problems, building smart user interfaces and useful interactions, 
+               developing rich web applications and seamless web experiences.</p>
+            <Button  />
+         </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TextHeader;

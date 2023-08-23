de7aba4ab566b6767a874f94a0f936d08dd10b16
Fatima <fatimahafeez.mh@gmail.com>
2023-08-23 13:51:09 -0400

    Update Textheader

--- src/components/text-header/Textheader.jsx
+++ src/components/text-header/Textheader.jsx
@@ -15,7 +15,6 @@ const TextHeader = () => {
            <h1>Front-End Developer</h1>
             <p>Resolving design problems, building smart user interfaces and useful interactions, 
                developing rich web applications and seamless web experiences.</p>
-            <Button  />
          </div>
         </div>
      </div>
